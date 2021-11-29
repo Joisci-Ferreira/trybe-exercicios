c46f046afda8dd9cf7410e450c1eff5bf0d9d5ba
Joiscilene <joisci.lene@hotmail.com>
2021-11-29 09:53:33 -0300

    Adiciona exercicios de fixação 1

---
+++ Fundamentos/bloco-04-introdução-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/exercicio_fixacao_01/script.js
@@ -0,0 +1,12 @@
+const myName = "Joiscilene";
+const birthCity = "Salvador";
+birthCity = "Feira de Santana"; 
+//Quando atribuimos uma nova constante o programa não roda porque a mesma não pode ser alterada
+
+let birthYear = 1995;
+birthYear = 2030;
+
+console.log (myName);
+console.log (birthCity);
+console.log (birthYear);
+
